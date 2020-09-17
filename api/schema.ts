957c5f5ca805e53c4730bd48709221b0b40f53cd
MJGTwo <gardner.j.michael@gmail.com>
2020-09-17 16:52:58 -0400

    added data models

--- api/schema.ts
+++ api/schema.ts
@@ -3,7 +3,7 @@ import { settings, log } from 'nexus';
 settings.change({
   server: {
     startMessage: (info) => {
-      log.info('Welcome! Now listening on:');
+      log.info('Welcome!');
       settings.original.server.startMessage(info);
     },
     port: +(process.env.PORT ?? 4000),
